660be10438eb3cafe9a78e361b29930fc8868815
oonray <alexanderbjornsrud@gmail.com>
2017-10-06 15:42:44 +0200

    Moved NN to PY

--- NNMNIST.py
+++ NNMNIST.py
@@ -1,15 +1,40 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
-
+from classes.hidden_layer import hidden_layer
 
 mnist = input_data.read_data_sets('./tmp', one_hot=True)
 
-x = tf.placeholder(tf.float32, shape=[None, 784])
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
+img_size = 28*28
 
-Weights = tf.Variable(tf.zeros([784,10]))
-Biases = tf.Variable(tf.zeros([10]))
+n_nodes_hidden_layer1 = 500
+n_nodes_hidden_layer2 = 500
+n_nodes_hidden_layer3 = 500
 
-with tf.Session() as sess:
-    pass
+n_classes = 10
+batch_size = 100
+
+x = tf.placeholder("float",[None,img_size])
+y = tf.placeholder("float")
+
+def nn_model(data):
+    layer1 = hidden_layer(n_nodes_hidden_layer1,img_size)
+    layer2 = hidden_layer(n_nodes_hidden_layer2,n_nodes_hidden_layer1)
+    layer3 = hidden_layer(n_nodes_hidden_layer3,n_nodes_hidden_layer2)
+    output = hidden_layer(n_classes,n_nodes_hidden_layer3)
+
+    return output.output(
+        layer3.nn(
+            layer2.nn(
+                layer1.nn(data))))
+
+
+def nn_train(inpt):
+    result = nn_model(inpt)
+    tf.reduce_mean(tf.nn.softmax(result,y))
+
+
+
+
+
+
 
