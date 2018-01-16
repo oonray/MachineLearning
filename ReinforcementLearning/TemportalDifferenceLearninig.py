20779374a9bc31d6a677c832361f496a7fb86977
oonray <alexanderbjornsrud@gmail.com>
2018-01-16 19:29:39 +0100

    RNN Up

--- ReinforcementLearning/TemportalDifferenceLearninig.py
+++ ReinforcementLearning/TemportalDifferenceLearninig.py
@@ -5,7 +5,7 @@ env = gym.make("CartPole-v1")
 
 State = []
 Last_Action = []
-Actions =  env.action_space.n
+Actions = env.action_space.n
 
 Time = 0
 
@@ -16,5 +16,22 @@ Discount = 0.8
 State.append(env.reset())
 
 for _ in range(games):
+    env.reset()
+    Done = False
     State_T = State[Time]
-    env.
+    env.render()
+    for _ in range(100):
+        action = env.action_space.sample()
+        state, reward, done, info = env.step(action)
+        Last_Action.append(action)
+        State_N = State_T+(LearningRate*((reward+Discount)*State_T-state))
+        State.append(State_N)
+        Time += 1
+        Done = done
+        if done:
+            break
+
+
+print([S for S in State])
+
+
