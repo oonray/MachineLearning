4bd78aa2e2190e670b5743dc6e0a1bb537e336bd
oonray <alexanderbjornsrud@gmail.com>
2018-04-08 22:50:34 +0200

    The Reinforcemnt reboot

--- ReinforcementLearning/T_CartPole.py
+++ ReinforcementLearning/T_CartPole.py
@@ -2,32 +2,62 @@ import gym
 import os
 import numpy as np
 import tensorflow as tf
-from .Classes.LinearControll import *
-
 
 env = gym.make("CartPole-v1")
 state = env.reset()
 
-action_space = env.action_space.n
-state_len = len(state)
 
+gamma = .9
+learning_rate = 5e10
+q = np.zeros(env.action_space.n)
 
-E_start_value = 1.0
-E_end_value = 0.1
-Lr_Start = 0.1
-Lr_Stop = 0.015
-Ep_Start = 5.0
-Ep_Stop = 10.0
-Iterations = 5e6
+memory = {}
+memory["q"] = np.zeros([100, len(q)])
+memory["state"] = np.zeros([100, len(state)])
 
-E = LinearControlSignal(E_start_value,E_end_value,num_iterations=1e6)
-LR = LinearControlSignal(Lr_Start,Lr_Stop,num_iterations=Iterations)
-Epocs = LinearControlSignal(Ep_Start,Ep_Stop,num_iterations=Iterations)
-UpdateIntervall =LinearControlSignal(E_start_value,E_end_value,num_iterations=Iterations)
+t=0
+done=False
 
+x = tf.placeholder("float")
+y = tf.placeholder("float")
 
-x = tf.placeholder(shape=[None,state_len],dtype=tf.float32)
-y = tf.placeholder(shape=[None,action_space],dtype=tf.float32)
+def model(x):
+    x = tf.cast(x, tf.float32)
+    w1 = tf.Variable(tf.random_normal([len(state), 1000]))
+    b1 = tf.Variable(tf.random_normal([1000]))
+    o1 = tf.nn.relu((tf.matmul(x, w1)+b1))
 
+    w2 = tf.Variable(tf.random_normal([1000, len(q)]))
+    b2 = tf.Variable(tf.random_normal([len(q)]))
+    return tf.matmul(o1,w2) + b2
 
+session = tf.Session()
 
+mod = model(x)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=mod, labels=y))
+optimiser = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+print("[+]Init.....")
+session.run(tf.global_variables_initializer())
+
+print("[+]Starting.....")
+try:
+    for _ in range(5):
+        while not done:
+            if t == len(memory["q"]):
+                print("[-]Memory full...")
+                break
+            else:
+                print("-"*20)
+                print("Time:",t)
+                q = session.run(mod, feed_dict={x: [state]})[0]
+                print("Q-Values:",q)
+                memory["state"][t], memory["q"][t] = (state,q)
+                action = q.argmax(axis=0)
+                print("Action:",action)
+                observation, reward, done, info = env.step(action)
+                state = observation
+                t+=1
+
+except KeyboardInterrupt:
+    pass
