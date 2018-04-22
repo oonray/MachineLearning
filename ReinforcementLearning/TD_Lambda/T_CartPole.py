241684c53055e3df4687e7824dadd18e7611f8fd
oonray <alexanderbjornsrud@gmail.com>
2018-04-22 23:06:55 +0200

    asdf

--- ReinforcementLearning/TD_Lambda/T_CartPole.py
+++ ReinforcementLearning/TD_Lambda/T_CartPole.py
@@ -4,39 +4,33 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 
-env = gym.make("CartPole-v1")
-state = env.reset()
+env = gym.make("MountainCar-v0")
 
 e = .9
-gamma = .9 #discount
-alpha = 5e10 #learingRate
+g = .9  #discount
+a = .1  #5e10 #learingRate
 
-Q = pd.DataFrame(np.zeros((1,env.action_space.n)), columns=range(env.action_space.n))
-print(Q)
+Q = pd.DataFrame(columns=range(env.action_space.n))
+
+def takeaction(state):
+    global Q
+    is_inTable(state)
+    print(Q)
+    if np.random.uniform() < e:
+        sa = Q.loc[state, :]
+        sa = sa.reindex(np.random.permutation(sa.index))
+        print(sa)
+        ret = sa.idxmax()
+    else:
+        ret = np.random.choice(env.action_space.n)
+    return ret
+
+def is_inTable(state):
+    global Q
+    if state not in Q.index:
+        Q = Q.append(pd.Series([0]*env.action_space.n, index=Q.columns, name=str(state)))
 
 
-"""
-x = tf.placeholder("float")
-y = tf.placeholder("float")
-
-def model(x):
-    x = tf.cast(x, tf.float32)
-    w1 = tf.Variable(tf.random_normal([len(state), 1000]))
-    b1 = tf.Variable(tf.random_normal([1000]))
-    o1 = tf.nn.relu((tf.matmul(x, w1)+b1))
-
-    w2 = tf.Variable(tf.random_normal([1000, len(q)]))
-    b2 = tf.Variable(tf.random_normal([len(q)]))
-    return tf.matmul(o1,w2) + b2
-
-session = tf.Session()
-
-mod = model(x)
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=mod, labels=y))
-optimiser = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-
-print("[+]Init.....")
-session.run(tf.global_variables_initializer())
-
-print("[+]Starting.....")
-"""
+action = takeaction(str(env.reset()))
+print(action)
+#env.step(action)
